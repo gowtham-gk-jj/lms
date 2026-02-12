91f5ba57f31ad850f48412d20671aff688588408
Your Name <ssaggowtham@gmail.com>
2026-02-12 17:29:11 +0530

    Fix axios import path

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,26 +1,23 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
-  // 1. Create a transporter
+
   const transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
+    host: process.env.MAILTRAP_HOST,
+    port: process.env.MAILTRAP_PORT,
     auth: {
-      user: "092778b0a9c8b2", // Paste from Mailtrap
-      pass: "dc1975f445fc82"  // Paste from Mailtrap
+      user: process.env.MAILTRAP_USER,
+      pass: process.env.MAILTRAP_PASS
     }
   });
 
-  // 2. Define email options
   const mailOptions = {
-    from: `"Support Team" <${process.env.EMAIL_USER}>`,
+    from: '"LMS Support" <no-reply@lms.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
-    // html: options.html (optional if you want fancy formatting)
   };
 
-  // 3. Send the email
   await transporter.sendMail(mailOptions);
 };
 
