bce564a588723de9f054efccabf205cfb805348d
Your Name <ssaggowtham@gmail.com>
2026-02-12 09:48:48 +0530

    Fix axios import path

--- frontend/src/services/notificationService.js
+++ frontend/src/services/notificationService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/notifications";
+const API_URL = "https://lms-dj4b.onrender.com";
 
 const getAuthHeader = () => {
   const token = localStorage.getItem("token");
